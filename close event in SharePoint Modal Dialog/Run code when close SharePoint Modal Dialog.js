ce5b03366738daf853251ff8c30b0eb904ec49cc
Mohamed El-Qassas <melqassas@outlook.com>
2020-02-15 03:50:21 +0300

    Update Run code when close SharePoint Modal Dialog.js

--- close event in SharePoint Modal Dialog/Run code when close SharePoint Modal Dialog.js
+++ close event in SharePoint Modal Dialog/Run code when close SharePoint Modal Dialog.js
@@ -1,21 +1,21 @@
 <script>
-function openDialog(pageUrl) {
-var options = {
-url: pageUrl,
-title: 'SPGeeks Ann',
-allowMaximize: false,
-showClose: true,
-width: 726,
-height: 372,
-dialogReturnValueCallback: function(result){
-
-if (result == SP.UI.DialogResult.cancel) {
-//Run code after closing  Modal dialog close
-//Redirect to external page
-window.location.href='https://debug.to';
-}
-}
-};
-SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
-}
+    function openDialog(pageUrl) {
+        var options = {
+            url: pageUrl,
+            title: 'SPGeeks Ann',
+            allowMaximize: false,
+            showClose: true,
+            width: 726,
+            height: 372,
+            dialogReturnValueCallback: function(result) {
+                // close event
+                if (result == SP.UI.DialogResult.cancel) {
+                    //Run code after closing  Modal dialog
+                    //Redirect to external page
+                    window.location.href = 'https://debug.to';
+                }
+            }
+        };
+        SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
+    } 
 </script>
