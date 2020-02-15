65aafd3e1356e999c5d1565f03f51d1b7600bb10
Mohamed El-Qassas <melqassas@outlook.com>
2020-02-15 03:53:07 +0300

    Update Open URL in SharePoint Modal Popup Dialog.js

--- Open URL in SharePoint Modal Popup Dialog/Open URL in SharePoint Modal Popup Dialog.js
+++ Open URL in SharePoint Modal Popup Dialog/Open URL in SharePoint Modal Popup Dialog.js
@@ -1,13 +1,13 @@
 <script>
-function openDialog(pageUrl) {
-var options = {
-url: pageUrl,
-title: 'Modal Title',
-allowMaximize: false,
-showClose: true,
-width: 726,
-height: 372
-};
-SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
-}
+	function openDialog(pageUrl) {
+		var options = {
+			url: pageUrl,
+			title: 'Modal Title',
+			allowMaximize: false,
+			showClose: true,
+			width: 726,
+			height: 372
+		};
+		SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
+	} 
 </script>
