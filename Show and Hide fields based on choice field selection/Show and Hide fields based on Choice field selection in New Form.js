95953eb7dec46e19c8351abde3047e4a29841b7c
Mohamed El-Qassas <melqassas@outlook.com>
2020-07-23 00:35:17 +0300

    Update Show and Hide fields based on Choice field selection in New Form.js

--- Show and Hide fields based on choice field selection/Show and Hide fields based on Choice field selection in New Form.js
+++ Show and Hide fields based on choice field selection/Show and Hide fields based on Choice field selection in New Form.js
@@ -1,13 +1,13 @@
-#Show and Hide fields based on Choice field selection in New Form
-<script src = "https://code.jquery.com/jquery-1.7.2.min.js" type = "text/javascript" > </script>
+//Show and Hide fields based on Choice field selection in New Form
+<script src = "https://code.jquery.com/jquery-1.7.2.min.js" type = "text/javascript"></script>
 <script type = "text/javascript">
     $(document).ready(function() {
         //Show/hide columns based on Drop Down Selection
-        $("select[title=’City’]").change(function() {
-            if ($("select[title=’City’]").val() != "other") {
-                $(‘nobr: contains("OtherCity")’).closest(‘tr’).hide();
+        $("select[title='City']").change(function() {
+            if ($("select[title='City']").val() != "other") {
+                $('nobr: contains("OtherCity")').closest('tr').hide();
             } else {
-                $(‘nobr: contains("OtherCity")’).closest(‘tr’).show();
+                $('nobr: contains("OtherCity")').closest('tr').show();
             }
         });
     }); 
