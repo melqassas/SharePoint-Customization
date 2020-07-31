a32d654583e3e28be0643ba45a0282de98518060
Mohamed El-Qassas <melqassas@outlook.com>
2020-07-31 22:31:49 +0300

    Create Close event in SharePoint Modal Dialog.js

---
+++ Open URL in SharePoint Modal Popup Dialog/Close event in SharePoint Modal Dialog.js
@@ -0,0 +1,22 @@
+// Close event in SharePoint Modal Dialog.js
+<script>
+    function openDialog(pageUrl) {
+        var options = {
+            url: pageUrl,
+            title: 'SPGeeks Ann',
+            allowMaximize: false,
+            showClose: true,
+            width: 726,
+            height: 372,
+            dialogReturnValueCallback: function(result) {
+                // close event
+                if (result == SP.UI.DialogResult.cancel) {
+                    //Run code after closing  Modal dialog
+                    //Redirect to external page
+                    window.location.href = 'https://debug.to';
+                }
+            }
+        };
+        SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
+    } 
+</script>
