ae5e082c0398904236502cad71f27ae3f5c5c55a
Mohamed El-Qassas <melqassas@outlook.com>
2020-12-02 13:49:39 +0300

    Create Highlight field cell.js

---
+++ Conditional Formatting SharePoint List Display Form/Highlight field cell.js
@@ -0,0 +1,15 @@
+<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        if ($('h3:contains("Project Status")').closest('td').next('td').text().indexOf('In Progress') != -1) {
+            $('h3:contains("Project Status")').closest('td').next('td').css("background-color", "green");
+            $('h3:contains("Project Status")').closest('td').next('td').css("color", "white");
+        } else if ($('h3:contains("Project Status")').closest('td').next('td').text().indexOf('Not Started') != -1) {
+            $('h3:contains("Project Status")').closest('td').next('td').css("background-color", "red");
+            $('h3:contains("Project Status")').closest('td').next('td').css("color", "white");
+        } else {
+            $('h3:contains("Project Status")').closest('td').next('td').css("background-color", "blue");
+            $('h3:contains("Project Status")').closest('td').next('td').css("color", "white");
+        }
+    }); 
+</script>
